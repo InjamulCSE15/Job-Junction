5442cc27c4d36a619fe1fdc8085fc6cd8deafc83
unknown <yeasin.mohammod018@gmail.com>
2024-07-10 22:27:11 +0600

    applied job and job card

---
+++ src/components/AppliedJobCard/AppliedJobCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export const AppliedJobCard = ({ appliedJob }) => {
+    const { id, job_title, company, logo, jobType, salary, position, remote, location } = appliedJob;
+    return (
+        <div className="p-5 border border-gray-200 rounded-md relative hover:-translate-y-1.5 transition-all duration-500 ease-in-out hover:border-indigo-500 group-data-[theme-color=sky]:hover:border-sky-500 group-data-[theme-color=red]:hover:border-red-500 group-data-[theme-color=green]:hover:border-green-500 group-data-[theme-color=pink]:hover:border-pink-500 group-data-[theme-color=blue]:hover:border-blue-500 hover:shadow-md hover:shadow-gray-100/30 dark:border-neutral-600 dark:hover:shadow-neutral-900">
+            <div className="grid grid-cols-12 items-center">
+                <div className="col-span-12 lg:col-span-1">
+                    <img alt={company} className="rounded-lg p-2 object-fill" src={logo} />
+                </div>
+                <div className="col-span-12 lg:col-span-9">
+                    <div className="mt-4 lg:mt-0">
+                        <h5 className="mb-1 text-base font-semibold text-gray-700 dark:text-gray-50">
+                            {job_title}
+                        </h5>
+                        <ul className="flex gap-3 mb-0">
+                            <li className="">
+                                <p className="mb-0 text-sm text-gray-500 dark:text-gray-300">
+                                    {company}
+                                </p>
+                            </li>
+                            <li className="">
+                                <p className="mb-0 inline-flex text-sm text-gray-500 dark:text-gray-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width="16" height="16" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                        <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                                    </svg>
+                                    {location}
+                                </p>
+                            </li>
+                            <li className="">
+                                <p className="mb-0 inline-flex text-sm font-semibold text-gray-500 dark:text-gray-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-wallet" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
+                                        <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
+                                    </svg>
+                                    {salary}
+                                </p>
+                            </li>
+                        </ul>
+                        <div className="flex flex-wrap gap-2 mt-3">
+                            <span className="px-2 py-0.5 mt-1 font-medium text-indigo-500 rounded bg-indigo-500/20 text-13">
+                                {position}
+                            </span>
+                            <span className="px-2 py-0.5 mt-1 font-medium text-cyan-500 rounded bg-cyan-500/20 text-13">
+                                {jobType}
+                            </span>
+                            <span className="px-2 py-0.5 mt-1 font-medium text-green-500 rounded bg-green-500/20 text-13">
+                                {remote ? 'Remote' : 'On-site'}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:text-right col-span-12 lg:col-span-2 ">
+                   <Link to={`job/${id}`} className='btn btn-primary text-white '>View Details</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,17 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
-import { getStoreAppliedJob } from '../LocalStore/localStore';
+import { getStoreAppliedJob, storeAppliedJobId } from '../LocalStore/localStore';
+import { AppliedJobCard } from '../AppliedJobCard/AppliedJobCard';
 
 export const AppliedJobs = () => {
   const jobs = useLoaderData();
+  const [appliedJobs, setAppliedJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
 
-  useEffect(()=>{
-    const storageJobIds = getStoreAppliedJob();
-    if(jobs.length > 0){
-      const jobsIapplied = jobs.filter(job => storageJobIds.includes(job.id))
-      console.log(jobsIapplied);
+  const filteringJob = (filter) => {
+    switch (filter) {
+      case 'remote':
+        const remoteJobs = appliedJobs.filter(job => job.remote == true);
+        setFilteredJobs(remoteJobs);
+        break;
+      case 'onsite':
+        const onsiteJobs = appliedJobs.filter(job => job.remote == false);
+        setFilteredJobs(onsiteJobs);
+        break;
+    
+      default:
+        setFilteredJobs(appliedJobs);
+        break;
     }
-  },[])
+  }
+
+  useEffect(() => {
+    const storageJobIds = getStoreAppliedJob();
+    if (jobs.length > 0) {
+      const tempJobsArr = [];
+      for (const id of storageJobIds) {
+        const job = jobs.find(job => job.id == id);
+        if (job) {
+          tempJobsArr.push(job);
+        }
+      }
+      setAppliedJobs(tempJobsArr);
+      setFilteredJobs(tempJobsArr);
+    }
+  }, [jobs])
 
   return (
     <div className="main-content">
@@ -32,103 +59,35 @@ export const AppliedJobs = () => {
 
         <section className="py-8">
           <div className="container mx-auto">
-            <div className="grid items-center grid-cols-12 mb-4">
+            <div className="flex items-center justify-between">
+              <h6 className="text-gray-900 text-base dark:text-gray-50">
+                Total applied: {appliedJobs.length}
+              </h6>
+              <div>
+                <details className="dropdown dropdown-end">
+                  <summary className="btn m-1">Filter by</summary>
+                  <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li onClick={()=>filteringJob('all')}><a>All</a></li>
+                    <li onClick={()=>filteringJob('remote')}><a>Remote</a></li>
+                    <li onClick={()=>filteringJob('onsite')}><a>On-site</a></li>
+                  </ul>
+                </details>                
+              </div>
+            </div>
+            {/* <div className="grid items-center grid-cols-12 mb-4">
               <div className="col-span-12 lg:col-span-8">
                 <div className="mb-3 mb-lg-0">
-                  <h6 className="text-gray-900 text-16 dark:text-gray-50">
-                    {' '}Showing 1 – 8 of 11 results{' '}
+                  <h6 className="text-gray-900 text-base dark:text-gray-50">
+                    Total applied: {appliedJobs.length}
                   </h6>
                 </div>
               </div>
-            </div>
-            <div className="mt-8">
+            </div> */}
+            <div className="mt-8 container mx-auto px-2">
               <div className="grid grid-cols-1 gap-y-5">
-                <div className="p-5 border border-gray-200 rounded-md relative hover:-translate-y-1.5 transition-all duration-500 ease-in-out hover:border-indigo-500 group-data-[theme-color=sky]:hover:border-sky-500 group-data-[theme-color=red]:hover:border-red-500 group-data-[theme-color=green]:hover:border-green-500 group-data-[theme-color=pink]:hover:border-pink-500 group-data-[theme-color=blue]:hover:border-blue-500 hover:shadow-md hover:shadow-gray-100/30 dark:border-neutral-600 dark:hover:shadow-neutral-900">
-                  <div className="grid grid-cols-12">
-                    <div className="col-span-12 lg:col-span-1">
-                      <a href="company-details.html">
-                        <img
-                          alt=""
-                          className="img-fluid rounded-3"
-                          src="assets/images/featured-job/img-01.png"
-                        />
-                      </a>
-                    </div>
-                    <div className="col-span-12 lg:col-span-9">
-                      <div className="mt-4 lg:mt-0">
-                        <h5 className="mb-1 text-17">
-                          <a
-                            className="text-gray-900 dark:text-gray-50"
-                            href="job-details.html"
-                          >
-                            Business Associate
-                          </a>
-                        </h5>
-                        <ul className="flex gap-3 mb-0">
-                          <li className="">
-                            <p className="mb-0 text-sm text-gray-500 dark:text-gray-300">
-                              Jobcy Technology Pvt.Ltd
-                            </p>
-                          </li>
-                          <li className="">
-                            <p className="mb-0 text-sm text-gray-500 dark:text-gray-300">
-                              <i className="mdi mdi-map-marker" />
-                              {' '}California
-                            </p>
-                          </li>
-                          <li className="">
-                            <p className="mb-0 text-sm text-gray-500 dark:text-gray-300">
-                              <i className="uil uil-wallet" />
-                              {' '}$250 - $800 / month
-                            </p>
-                          </li>
-                        </ul>
-                        <div className="flex flex-wrap gap-2 mt-3">
-                          <span className="px-2 py-0.5 mt-1 font-medium text-red-500 rounded bg-red-500/20 text-13">
-                            Part Time
-                          </span>
-                          <span className="px-2 py-0.5 mt-1 font-medium text-yellow-500 rounded bg-yellow-500/20 text-13">
-                            Urgent
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="items-center col-span-12 lg:col-span-2 ">
-                      <ul className="flex flex-wrap gap-3 mt-4 lg:mt-0">
-                        <li
-                          aria-label="Edit"
-                          className="w-10 h-10 text-lg leading-10 text-center text-green-500 rounded-full bg-green-500/20"
-                          data-bs-original-title="Edit"
-                          data-bs-placement="top"
-                          data-bs-toggle="tooltip"
-                        >
-                          <a
-                            className="text-center avatar-sm success-bg-subtle d-inline-block rounded-circle fs-18"
-                            href="manage-jobs-post.html"
-                          >
-                            <i className="uil uil-edit" />
-                          </a>
-                        </li>
-                        <li
-                          aria-label="Delete"
-                          className="w-10 h-10 text-lg leading-10 text-center text-red-500 rounded-full bg-red-500/20"
-                          data-bs-original-title="Delete"
-                          data-bs-placement="top"
-                          data-bs-toggle="tooltip"
-                        >
-                          <a
-                            className="text-center avatar-sm danger-bg-subtle d-inline-block rounded-circle fs-18"
-                            data-bs-target="#deleteModal"
-                            data-bs-toggle="modal"
-                            
-                          >
-                            <i className="uil uil-trash-alt" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
+                {
+                  filteredJobs.map(appliedJob => <AppliedJobCard key={appliedJob.id} appliedJob={appliedJob}></AppliedJobCard>)
+                }
               </div>
             </div>
           </div>
